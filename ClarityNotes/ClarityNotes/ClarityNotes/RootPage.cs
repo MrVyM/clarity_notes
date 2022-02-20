5ecea51049be7f1d4cc12e8597d74fb5e212b2de
Vianney Marticou <la.regle.du.jeu.ytb@gmail.com>
2022-02-20 15:01:15 +0100

    Mise en place des premier bouton via une boucle for

--- ClarityNotes/ClarityNotes/ClarityNotes/RootPage.cs
+++ ClarityNotes/ClarityNotes/ClarityNotes/RootPage.cs
@@ -11,12 +11,47 @@ namespace ClarityNotes
     {
         public RootPage()
         {
-            Content = new StackLayout
+
+            Label testLabel = new Label();
+
+            testLabel.Text = "test";
+
+            StackLayout mainContent = new StackLayout();
+            mainContent.Orientation = StackOrientation.Horizontal;
+            mainContent.VerticalOptions = LayoutOptions.Start;
+            mainContent.HorizontalOptions = LayoutOptions.Start;
+
+
+            Frame frameColumn = new Frame();
+            frameColumn.VerticalOptions = LayoutOptions.StartAndExpand;
+            frameColumn.HorizontalOptions = LayoutOptions.StartAndExpand;   
+
+
+            StackLayout verticalColumn = new StackLayout();
+            verticalColumn.HorizontalOptions = LayoutOptions.Center;
+            verticalColumn.VerticalOptions = LayoutOptions.StartAndExpand;
+            verticalColumn.Margin = 10;
+
+
+            for (int i = 0; i < 10; i++)
             {
-                Children = {
-                    new Label { Text = "Welcome to Xamarin.Forms!" }
-                }
-            };
+                Button but = new Button();
+                but.Text = i.ToString();
+                but.WidthRequest = 50;
+                but.Clicked += OnButtonCliked;
+                verticalColumn.Children.Add(but);
+            }
+
+            frameColumn.Content = verticalColumn;
+
+            mainContent.Children.Add(frameColumn);
+            mainContent.Children.Add(testLabel);
+            this.Content = mainContent;
+        }
+
+        private void OnButtonCliked(object sender, EventArgs e)
+        {
+            Console.WriteLine(((Button)sender).Text);
         }
     }
 }
