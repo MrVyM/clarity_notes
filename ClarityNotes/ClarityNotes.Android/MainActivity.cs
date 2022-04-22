399cea2b7e0d6f59dfc220cf863747f9bf607c22
Romain VALLETTE--GRISEL <romain.vallette-grisel@epita.fr>
2022-04-22 21:01:55 +0200

    Changement du logo par défaut de l'application Android

--- ClarityNotes/ClarityNotes.Android/MainActivity.cs
+++ ClarityNotes/ClarityNotes.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace ClarityNotes.Droid
 {
-    [Activity(Label = "ClarityNotes", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ClarityNotes", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
