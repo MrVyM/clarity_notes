4bea3420799999643f706ee66938098f168f4707
Vianney Marticou <vianney.marticou@epita.fr>
2022-04-25 09:55:11 -0700

    Mise en page deleteAccount

--- ClarityNotes/ClarityNotes/Pages/DeleteAccount.cs
+++ ClarityNotes/ClarityNotes/Pages/DeleteAccount.cs
@@ -15,29 +15,37 @@ namespace ClarityNotes
         {
             this.user = user;
             StackLayout mainContent = new StackLayout();
+            mainContent.HorizontalOptions = LayoutOptions.Center;
+            mainContent.VerticalOptions = LayoutOptions.Center;
+
+            Frame frame = new Frame();
+            StackLayout framStack = new StackLayout();
+            
+            Label texte = new Label();
+            texte.Text = "Veuillez entrer votre mot de passe";
+            framStack.Children.Add(texte);
 
             password = new Entry();
             password.IsPassword = true;
-            mainContent.Children.Add(password);
+            framStack.Children.Add(password);
+
+            frame.Content = framStack;
+            mainContent.Children.Add(frame);
 
             Button delete = new Button();
             delete.Text = "Suprimer";
             delete.Clicked += OnDelete;
             mainContent.Children.Add(delete);
 
+
+            this.BackgroundColor = Color.FromHex("57b1eb");
             this.Title = "Supression de compte";
             this.Content = mainContent;
         }
         private void OnDelete(object sender, EventArgs e)
         {
             User.DeleteUser(user.Id);
-            foreach (var page in Navigation.ModalStack)
-            {
-                if (page != this)
-                    Navigation.RemovePage(page);
-            }
-            Navigation.PushAsync(new LoginPage());
-            Navigation.RemovePage(this);
+            Navigation.PopToRootAsync();
         }
     }
 }
