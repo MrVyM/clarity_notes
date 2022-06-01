94172f07fe9a42b7598c1daabdcbd90f0ae77d85
khoren <khoren.pasdr@gmail.com>
2022-06-01 21:35:37 +0200

    fix

--- ClarityNotes/ClarityNotes/Pages/InteractionBDD/Notes/EditorPage.cs
+++ ClarityNotes/ClarityNotes/Pages/InteractionBDD/Notes/EditorPage.cs
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 ﻿using System;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -15,79 +13,6 @@ namespace ClarityNotes
         User user;
         SfRichTextEditor editor;
         private string projectID;
-
-        public SfRichTextEditor _editor => editor;
-
-
-        public EditorPage(Note note, User user)
-        {
-            this.note = note;
-            this.user = user;
-
-            Button traduce = new Button
-            {
-                Text = "traduire",
-                HeightRequest = 50,
-                WidthRequest = 70
-            };
-
-            traduce.Clicked += OnTraduceCliked; 
-
-
-            editor = new SfRichTextEditor();
-            editor.AutoSize = AutoSizeOption.TextChanges;
-            editor.VerticalOptions = LayoutOptions.CenterAndExpand;
-            editor.HtmlText = note.Content;
-            editor.HeightRequest = 1000;
-            editor.PlaceHolder = "Votre note";
-            editor.TextChanged += OnTextChanged;
-
-            if (editor.ToolbarItems.Count == 1)
-                editor.ToolbarItems.Add(traduce);
-            else
-                editor.ToolbarItems[1] = traduce;
-
-            StackLayout stack = new StackLayout();
-            stack.VerticalOptions = LayoutOptions.Start;
-            stack.Children.Add(editor);
-            this.Content = stack;
-        }
-
-        public void OnTextChanged(object sender, Syncfusion.XForms.RichTextEditor.TextChangedEventArgs e)
-        {
-            note.Update(editor.HtmlText, user);
-            string text = e.Text;
-        }
-
-        public void OnTraduceCliked(object sender, EventArgs e)
-        {
-            TranslationServiceClient client = TranslationServiceClient.Create();
-            TranslateTextRequest request = new TranslateTextRequest
-            {
-                Contents = { this.editor.Text },
-                TargetLanguageCode = "fr-FR",
-                Parent = new ProjectName(projectID).ToString()
-            };
-            TranslateTextResponse response = client.TranslateText(request);
-            Translation translation = response.Translations[0];
-            this.editor.Text = translation.TranslatedText;
-        }
-    }
-=======
-using System;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-using Syncfusion.XForms.RichTextEditor;
-
-
-namespace ClarityNotes
-{
-    public class EditorPage : ContentPage
-    {
-        Note note;
-        User user;
-        SfRichTextEditor editor;
-
 
         public EditorPage(Note note, User user)
         {
@@ -138,7 +63,8 @@ namespace ClarityNotes
             };
             TranslateTextResponse response = client.TranslateText(request);
             Translation translation = response.Translations[0];
-            this.editor.Text += translation.TranslatedText;
+            string translateText = translation.TranslatedText;
+            this.editor.Text = translateText; 
         }
     }
 }
